4b1d0c03810bec9af83bff097ab4d06aa93605e0
Alireza0K <alireza.karimi.programmer@gmail.com>
2023-07-17 08:55:15 +0330

    add elements & normal password creator

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+from random import shuffle
+
 print("""
       Hello there, welcome to first Alireza0K python console app. \n
       this is strong password generator, this console app can make best password for you.\n
@@ -7,3 +9,53 @@ print("""
       say thank you for useing this app. \n
       """)
 
+upperCaseAlphabet = "abcdefghijklmnopqrstuvwxyz" # list of Elements
+
+lowerCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+numbers = "1234567890"
+
+symbols = "?><}{:!@#$%^&*()_+=-|\\/"
+
+passwordLimit = int(input()) # password limit 'user given'
+
+# shuffled creator
+def ShuffleJoiner(thingList):
+    
+    thingList = list(thingList)
+    
+    shuffle(thingList)
+    
+    thing = "".join(thingList)
+    
+    return thing
+
+# limiter make a priod of a string
+def Limiter(limit, stringOfThings):
+    
+    result = stringOfThings[0:limit]
+    
+    return result
+
+# normal password creator
+def NormalPassword(limit, elements):
+    
+    rowPassword = ""
+    
+    lengthOfElements = len(elements)
+    
+    for element in elements:
+        
+        rowPassword += element
+        
+        if "1" in element:
+            
+           rowPassword += element 
+           
+    rowPassword = ShuffleJoiner(rowPassword)
+    
+    lastResult = Limiter(limit, rowPassword)
+    
+    return lastResult
+
+print(NormalPassword(passwordLimit, [upperCaseAlphabet,numbers]))
