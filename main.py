38090ccf573eb2573f621da32b4f9895560ad783
Alireza0K <alireza.karimi.programmer@gmail.com>
2023-07-17 17:59:01 +0330

    complete console app side of password list

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ print("""
            enter the limit you want. ^Attention! normal password created just with alphabets and numbers.^\n
       \t2. if you wana to make a Strong password, first enter the 'S' and second
            enter the limit you want. ^Attention! strong password created with alphabets, symbols and numbers.^\n
+      \t3. if you wana to make a password list, first enter the 'L' and second enter
+           limit you want,before choose the elements enter limit of length of passwords,
+           enter elements you want for passwords [upper Case Alphabet,
+           lower Case Alphabet, numbers, symbols] and after you finished write 'ex'.
       """)
 
-upperCaseAlphabet = "abcdefghijklmnopqrstuvwxyz" # list of Elements
+upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # list of Elements
 
-lowerCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
 
 numbers = "1234567890"
 
@@ -82,6 +86,31 @@ def StrongPassword(limit, elements):
     
     return lastResult
 
+# password list creator
+def PasswordList(limit, length, elements):
+    
+    rowPassword = ""
+    
+    for element in elements:
+        
+        rowPassword += element
+        
+        if "1" in element:
+            
+           rowPassword += element 
+    
+    listOfPassword = []
+    
+    for i in range(0, limit):
+        
+        rowPassword = ShuffleJoiner(rowPassword)
+    
+        result = Limiter(length, rowPassword)
+        
+        listOfPassword.append(result)
+    
+    return listOfPassword
+
 # conditions for recognise to create which type password
 if userChose == "N":
     
@@ -90,3 +119,35 @@ if userChose == "N":
 elif userChose == "S":
     
     print("you're password is:",NormalPassword(passwordLimit, [upperCaseAlphabet,numbers,symbols,lowerCaseAlphabet]))
+
+elif userChose == "L":
+    
+    passwordCase = []
+    
+    passwordLength = int(input("Enter your passwords length: "))
+    
+    while True:
+        
+        userElements = str(input("choose your Elements: "))
+        
+        if userElements == "ex":
+            
+            break
+        
+        elif userElements == "upper Case Alphabet":
+            
+            passwordCase.extend(upperCaseAlphabet)
+        
+        elif userElements == "lower Case Alphabet":
+            
+            passwordCase.extend(lowerCaseAlphabet)
+        
+        elif userElements == "numbers":
+            
+            passwordCase.extend(numbers)
+        
+        elif userElements == "symbols":
+            
+            passwordCase.extend(symbols) 
+    
+    print(PasswordList(passwordLimit, passwordLength, passwordCase))
